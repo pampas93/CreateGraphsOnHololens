85b2ee992ed64abbefc77b48eab650ba0cfd91e1
pampas93 <abhijit.93@hotmail.com>
2017-07-30 22:26:33 -0400

    Axis Labels + corrected bugs on bar Y position

--- Assets/Scripts/PlotBars.cs
+++ Assets/Scripts/PlotBars.cs
@@ -11,6 +11,7 @@ public class PlotBars : MonoBehaviour {
 
     private int ticks = 5;
     private int eachSegHeight;
+    private int yMax;
 
     private string[] titles;
 
@@ -40,7 +41,12 @@ public class PlotBars : MonoBehaviour {
         //Debug.Log(centerPos.y - extents.y);
 
         addToDictionary();
-        
+
+        yMax = calculateAxis();
+        drawAxis();
+        //DrawAxisLabels dxObj = new DrawAxisLabels(ticks, yMax);
+        //dxObj.drawAxis();   //Creates Axis and labels
+
         if (data_set.Count < 7)
             plotGraph();
         else
@@ -66,16 +72,16 @@ public class PlotBars : MonoBehaviour {
             //}
 
             //Adding data into dictionary manually
-            data_set.Add("Alexandra's Mist", 51.8f);
-            data_set.Add("Axe Capital", 32.4f);
-            data_set.Add("Caribbean Cowboy", 48.2f);
-            data_set.Add("Cass's Dovehunt", 40f);
-            data_set.Add("Chessen", 39.6f);
-            data_set.Add("Pemps Cowboy", 28.2f);
-            data_set.Add("Mind body soul", 50f);
-            data_set.Add("Axe", 32.4f);
+            data_set.Add("Alexandra's Mist", 40f);
+            data_set.Add("Axe Capital", 35f);
+            data_set.Add("Caribbean Cowboy", 15f);
+            data_set.Add("Cass's Dovehunt", 20f);
+            data_set.Add("Chessen", 25f);
+            data_set.Add("Pemps Cowboy", 30f);
+            data_set.Add("Mind body soul", 5f);
+            data_set.Add("Axe", 120.4f);
             data_set.Add("Cowboy", 48.2f);
-            data_set.Add("Dovehunt", 40f);
+            //data_set.Add("Dovehunt", 40f);
             //data_set.Add("Caribbean", 48.2f);     //Extra data just to test
             //data_set.Add("Cass's", 40f);
             //data_set.Add("sen", 39.6f);
@@ -102,10 +108,41 @@ public class PlotBars : MonoBehaviour {
 
     }
 
+    private void drawAxis()
+    {
+        var gap = (float)1.0f / ticks;
+        float initialPosition = -0.5f;
+        var valueGap = yMax / ticks;
+
+        for (int i = 0; i <= ticks; i++)
+        {
+            GameObject cross_axis = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cross_axis.transform.parent = this.gameObject.transform;
+            cross_axis.name = "crossAxis";
+
+            Vector3 axisScale = new Vector3(1.0f, 0.005f, 0.005f);
+            cross_axis.transform.localScale = axisScale;
+
+            Vector3 axisPosition = new Vector3(0.0f, initialPosition, 0.5f);
+            cross_axis.transform.localPosition = axisPosition;
+
+            //Creating textMesh Gameobject also;
+            var axisValue = i * valueGap;
+
+            GameObject textObj = new GameObject(axisValue.ToString());
+            textObj.transform.parent = this.gameObject.transform;
+            var t = textObj.AddComponent<TextMesh>();
+            t.text = axisValue.ToString();
+            textObj.transform.localScale = new Vector3(0.02f, 0.05f, 0.05f);
+            textObj.transform.localPosition = new Vector3(-0.5f, initialPosition, 0.5f);
+
+            initialPosition = initialPosition + gap;
+        }
+    }
+
+
     private void plotGraph()
     {
-        int yMin = 0;
-        int yMax = calculateAxis();
         //Debug.Log(yMax);
         float barWidth = 0.1f;
 
@@ -117,18 +154,13 @@ public class PlotBars : MonoBehaviour {
             bar.name = item.Key;
 
             float bar_height = item.Value / yMax;
-
             
             Vector3 cubeScale = new Vector3(barWidth, bar_height, barWidth*10);     //z scale = barWidth*10 only to keep the bar depth also proportional to bar
             bar.transform.localScale = cubeScale;
 
-            //To position  the bar on top of X axis
-            var barSize = bar.GetComponent<Renderer>().bounds.size; 
-            var barExtents = bar.GetComponent<Renderer>().bounds.extents;
-            var temp = barSize.y + bottomAxisPos;
+            //To position  the bar on top of X axis;
             //Debug.Log(barExtents.y);
-
-            var yPos = temp - barExtents.y;
+            var yPos = -0.5f + (bar_height / 2);
             Vector3 cubePos = new Vector3(initial_pos, yPos, -0.2f);
             bar.transform.localPosition = cubePos;
             
@@ -140,12 +172,10 @@ public class PlotBars : MonoBehaviour {
     private void plotSmallerGraph()
     {
         Debug.Log("Executing bigger graph");
-        int yMin = 0;
-        int yMax = calculateAxis();
-        initial_pos = -0.45f;
 
         //need to recalculate bar_gap and barWidth based on number of items
         float barWidth = CalculateBarWidth();
+        initial_pos = -0.45f + bar_gap;
 
         foreach (var item in data_set)
         {
@@ -158,14 +188,10 @@ public class PlotBars : MonoBehaviour {
             Vector3 cubeScale = new Vector3(barWidth, bar_height, barWidth*10);
             bar.transform.localScale = cubeScale;
 
-            var barSize = bar.GetComponent<Renderer>().bounds.size;
-            var barExtents = bar.GetComponent<Renderer>().bounds.extents;
-            var temp = barSize.y + bottomAxisPos;
-
-            var yPos = temp - barExtents.y;
+            //Debug.Log(bar_height);
+            var yPos = -0.5f + (bar_height / 2);
             Vector3 cubePos = new Vector3(initial_pos, yPos, -0.2f);
             bar.transform.localPosition = cubePos;
-
 
             initial_pos = initial_pos + bar_gap + barWidth;
         }
